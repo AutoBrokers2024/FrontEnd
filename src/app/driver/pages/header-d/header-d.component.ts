e15fa984615e9e58537ea6b5f549359088fbe54d
AndresDoig0711 <andres.doig92@gmail.com>
2024-06-26 21:07:46 -0500

    updated header

--- src/app/driver/pages/header-d/header-d.component.ts
+++ src/app/driver/pages/header-d/header-d.component.ts
@@ -1,15 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-header-d',
   templateUrl: './header-d.component.html',
   styleUrls: ['./header-d.component.css']
 })
-export class HeaderDComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
-}
+export class HeaderDComponent { }
